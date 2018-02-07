c837077b6e5224a258ac616200c24822b48c7cfc
Ryo Ota <nwtgck@gmail.com>
2018-02-07 18:51:43 +0900

    [add] Add auto-generated stack files

---
+++ README.md
@@ -0,0 +1,9 @@
+# platy-lang
+
+
+## Run
+
+```bash
+stack build
+stack exec platy-lang-exe
+```
---
+++ app/Main.hs
@@ -0,0 +1,6 @@
+module Main where
+
+-- TODO: Implement
+main :: IO ()
+main = do
+  putStrLn "hello, world from stack executable template"
