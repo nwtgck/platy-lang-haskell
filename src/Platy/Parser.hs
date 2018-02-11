72254504519feee392e1768289eb9c99bc64238e
Ryo Ota <nwtgck@gmail.com>
2018-02-11 21:19:49 +0900

    [add/test] Add parsers and Add tests for them

---
+++ src/Platy/Parser.hs
@@ -0,0 +1,159 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE NamedFieldPuns #-}
+
+-- TODO: Don't expose all function
+module Platy.Parser where
+
+import           Text.Parsec      (Parsec, (<|>))
+import qualified Text.Parsec      as Parsec
+import qualified Text.Parsec.Char as ParsecChar
+
+import Platy.Datatypes
+
+-- | Prefix of keywords (reserved world)
+keywordPrefixChar :: Char
+keywordPrefixChar = '@'
+
+-- | One-line comment char
+onelineCommentChar = ';'
+
+-- | Int type name
+intTyNameStr = "Int"
+
+-- | Bool type name
+boolTyNameStr = "Bool"
+
+-- | Char type name
+charTyNameStr = "Char"
+
+-- | Unit type name
+unitTyNameStr = "Unit"
+
+-- | Unit literal name
+unitLitStr    = "Unit"
+
+-- | True literal name
+trueLitStr    = "True"
+
+-- | False literal name
+falseLitStr   = "False"
+
+
+-- | Consume a white or comment
+skipLangSpaceP :: Parsec String u ()
+skipLangSpaceP = (Parsec.space *> return ()) <|> do
+  Parsec.char onelineCommentChar
+  Parsec.manyTill Parsec.anyChar (Parsec.endOfLine *> return () <|> Parsec.eof)
+  return ()
+
+-- | Between '(' and ')'
+betweenParens :: Parsec String u a -> Parsec String u a
+betweenParens = Parsec.between (ParsecChar.char '(' >> Parsec.skipMany skipLangSpaceP) (Parsec.skipMany skipLangSpaceP >> ParsecChar.char ')')
+
+-- | List of p Parser
+listP :: Parsec String u a -> Parsec String u [a]
+listP p = do
+  ParsecChar.char '['
+  Parsec.skipMany skipLangSpaceP
+  as <- Parsec.sepBy p (Parsec.skipMany1 skipLangSpaceP)
+  Parsec.skipMany skipLangSpaceP
+  ParsecChar.char ']'
+  return as
+
+
+
+
+-- | Parser of identifier
+identP :: Parsec String u Ident
+identP = do
+  start <- ParsecChar.letter <|> ParsecChar.char '_'
+  rest  <- Parsec.many (ParsecChar.alphaNum <|> ParsecChar.char '_')
+  return $ Ident (start:rest)
+
+
+-- | Parser of type
+tyP :: Parsec String u Ty
+tyP = intTyP <|> boolTyP <|> charTyP <|> unitTyP
+  where
+    intTyP  = ParsecChar.string intTyNameStr  >> return IntTy
+    boolTyP = ParsecChar.string boolTyNameStr >> return BoolTy
+    charTyP = ParsecChar.string charTyNameStr >> return CharTy
+    unitTyP = ParsecChar.string unitTyNameStr >> return UnitTy
+
+-- | Parser of literal
+litP :: Parsec String u Lit
+litP = intLitP <|> boolLitP <|> charLitP <|> unitLitP
+  where
+    intLitP = do
+      intStr <- Parsec.many1 (ParsecChar.digit)
+      return (IntLit (read intStr))
+
+    boolLitP = trueLitP <|> falseLitP
+      where
+        trueLitP  = ParsecChar.string trueLitStr  >> return (BoolLit True)
+        falseLitP = ParsecChar.string falseLitStr >> return (BoolLit False)
+
+    charLitP = do
+      ParsecChar.char '\''
+      ch <- ParsecChar.anyChar
+      ParsecChar.char '\''
+      return (CharLit ch)
+
+    unitLitP = ParsecChar.string unitLitStr >> return UnitLit
+
+-- | Parser of expression
+exprP :: Parsec String u Expr
+exprP = Parsec.try litExprP <|> identExprP <|> betweenParens (ifExprP <|> applyExprP)
+  where
+    -- | Parser of literal expression
+    litExprP = do
+      lit <- litP
+      return $ LitExpr lit
+
+    -- | Parser of identifier expression
+    identExprP = do
+      ident <- identP
+      return $ IdentExpr ident
+
+    -- | Parser of if expression
+    ifExprP = do
+      -- @if
+      ParsecChar.string (keywordPrefixChar:"if") -- TODO: Hard code
+      Parsec.skipMany1 skipLangSpaceP
+      -- condition expression
+      condExpr <- exprP
+      Parsec.skipMany1 skipLangSpaceP
+      -- then expression
+      thenExpr <- exprP
+      Parsec.skipMany1 skipLangSpaceP
+      -- else expression
+      elseExpr <- exprP
+      return IfExpr {condExpr, thenExpr, elseExpr}
+
+    -- | Parser of apply expression
+    applyExprP = do
+      calleeIdent <- identP
+      Parsec.skipMany1 skipLangSpaceP
+      argExprs <- listP exprP
+      return ApplyExpr {calleeIdent, argExprs}
+
+
+-- | Parser of Global Definition
+gdefP :: Parsec String u Gdef
+gdefP = betweenParens (ParsecChar.char keywordPrefixChar *> (letGdefP <|> funcGdefP))
+  where
+    letGdefP  = do
+      -- @global-let
+      ParsecChar.string "global-let" -- TODO: Hard code
+      Parsec.skipMany1 skipLangSpaceP
+      -- identifier
+      ident <- identP
+      Parsec.skipMany1 skipLangSpaceP
+      -- type
+      ty   <- tyP
+      Parsec.skipMany1 skipLangSpaceP
+      -- expression
+      bodyExpr <- exprP
+      return $ LetGdef {bind=Bind{ident, ty, bodyExpr}}
+
+    funcGdefP = undefined -- TODO: Impl
---
+++ test/Platy/ParserSpec.hs
@@ -0,0 +1,70 @@
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE QuasiQuotes #-}
+
+module Platy.ParserSpec where
+
+import           Test.Hspec
+import           Test.Hspec.QuickCheck
+import           Test.QuickCheck
+
+import qualified Data.String.Here as Here
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.Text.Lazy.IO as TIO
+import qualified Data.ByteString as ByteString
+import qualified Data.Map as Map
+import Data.Map (Map)
+
+import qualified LLVM.Pretty
+
+import Debug.Trace
+
+import Platy.Datatypes
+import Platy.Codegen
+import Platy.Utils
+import Platy.Parser
+import qualified Platy.TestUtils as  TestUtils
+
+
+import           Text.Parsec      (Parsec, (<|>))
+import qualified Text.Parsec      as Parsec
+import qualified Text.Parsec.Char as ParsecChar
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+  describe "Parse Expression Test" $ do
+    it "23 - Int literal" $ do
+      let expect = Right $ LitExpr $ IntLit 23
+      Parsec.parse exprP "" "23" `shouldBe` expect
+
+    it "'k' - Char literal" $ do
+      let expect = Right $ LitExpr $ CharLit 'k'
+      Parsec.parse exprP "" "'k'" `shouldBe` expect
+
+    it "'myVar' - Identifier" $ do
+      let expect = Right $ IdentExpr $ Ident "myVar"
+      Parsec.parse exprP "" "myVar" `shouldBe` expect
+
+    it "(myfunc [78]) - Apply" $ do
+      let expect = Right $ ApplyExpr {calleeIdent=(Ident "myfunc"), argExprs=[LitExpr (IntLit 78)]}
+      Parsec.parse exprP "" "(myfunc [78])" `shouldBe` expect
+
+    it "(@if True 5544 2299)" $ do
+      let expect = Right $ IfExpr {condExpr=LitExpr $ BoolLit True, thenExpr=LitExpr $ IntLit 5544, elseExpr=LitExpr $ IntLit 2299}
+      Parsec.parse exprP "" "(@if True 5544 2299)" `shouldBe` expect
+
+
+  describe "Parse Global Definition" $ do
+    it "@global-let" $ do
+      let expect = Right $ LetGdef
+                           { bind =
+                             Bind
+                             { ident = Ident "myGlobal"
+                             , ty = IntTy
+                             , bodyExpr = LitExpr $ IntLit 445544
+                             }
+                           }
+      Parsec.parse gdefP "" "(@global-let myGlobal Int 445544)" `shouldBe` expect
