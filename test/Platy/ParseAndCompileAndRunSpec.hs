b7c6dfe1d2ddafeaf4c80e4d38a851c45f54e79b
Ryo Ota <nwtgck@gmail.com>
2018-03-09 19:44:32 +0900

    [fix/test] Fix IR generation of nested-if expression and test it

--- test/Platy/ParseAndCompileAndRunSpec.hs
+++ test/Platy/ParseAndCompileAndRunSpec.hs
@@ -41,6 +41,7 @@ spec = do
                           , "fib"
                           , "print_negative_int"
                           , "nested_let"
+                          , "nested_if"
                           ] -- NOTE: All you have to do is adding here
     let platyExtension  = "platy"
     let expectExtension = "expect"
